a0bea7f98fbb5df3c1e5bbe4ba64d655e05765ad
Aik-202 <ikogweassurance85@gmail.com>
2024-02-12 13:43:48 +0100

    Installed react router dom, exported pages, added routes, added logo image for favicon

--- musa-daniel/src/App.jsx
+++ musa-daniel/src/App.jsx
@@ -1,34 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css'
+import { AboutPage, ContactPage, HomePage, ProjectDetailPage, WorkPage } from './Pages';
 
 export default function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={ <HomePage /> }></Route>
+        <Route exact path='/about' element={ <AboutPage /> }></Route>
+        <Route exact path='/works' element={ <WorkPage /> }></Route>
+        <Route exact path='/contact' element={ <ContactPage /> }></Route>
+        <Route exact path='/:id' element={ <ProjectDetailPage /> }></Route>
+      </Routes>
+    </Router>
+  </>
   )
 }
 

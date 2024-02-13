fb39b6a21907d754616546bf078b8234e96da789
Aik-202 <ikogweassurance85@gmail.com>
2024-02-13 13:37:31 +0100

    Added a new FeaturedWork component, added it's html, and styled it

---
+++ musa-daniel/src/Components/FeaturedWork.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { featuredWorks } from '../Data'
+import { useNavigate } from 'react-router-dom'
+
+export default function FeaturedWork() {
+    const navigate = useNavigate();
+
+  return (
+    <section className='flex flex-col space-y-14 justify-center items-center cursor-pointer'>
+        <h2 className='font-bold text-3xl hover:text-[#27AE60]' 
+        onClick={() => navigate('/works')}>Featured works</h2>
+        <img src={featuredWorks} alt="featured-works" className='self-center w-[90%] ' 
+        onClick={() => navigate('/works')} />
+    </section>
+  )
+}
--- musa-daniel/src/Data/index.jsx
+++ musa-daniel/src/Data/index.jsx
@@ -3,3 +3,5 @@ export {default as daniel} from './images/main/daniel.png'
 export {default as ura} from './images/main/ura-icon.png'
 export {default as pi} from './images/main/pi-icon.png'
 export {default as ut} from './images/main/ut-icon.png'
+
+export {default as featuredWorks} from './images/main/featured-works.png'

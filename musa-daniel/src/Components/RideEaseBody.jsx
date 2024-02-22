bb9462548294f3a66ae80d0e99c625867d4264fe
Aik-202 <ikogweassurance85@gmail.com>
2024-02-22 09:22:56 +0100

    Made the first section of the ride ease body component responsive across all break points

--- musa-daniel/src/Components/RideEaseBody.jsx
+++ musa-daniel/src/Components/RideEaseBody.jsx
@@ -7,22 +7,25 @@ export default function RideEaseBody() {
     <section className='flex flex-col space-y-10 justify-center items-center w-full'>
         <section className='flex flex-col-reverse space-y-reverse space-y-10 md:space-y-0
         md:flex-row md:justify-between w-full px-5'>
-            <div className='relative flex flex-col space-y-8 pt-20 lg:pt-44 md:pl-10 h-max'>
+            <div className='relative flex flex-col space-y-8 mx-auto md:mx-0 pt-20 lg:pt-44 
+            md:pl-10'>
                 <div className='flex flex-col space-y-2'>
-                    <h2 className='font-five font-bold text-3xl md:text-2xl lg:text-4xl'>
+                    <h2 className='font-five font-bold text-2xl md:text-xl lg:text-4xl'>
                     RideEase</h2>
-                    <p className='font-three text-xl md:text-sm lg:text-2xl font-medium'>
+                    <p className='font-three text-lg md:text-sm lg:text-2xl font-medium'>
                     A Ride-Sharing App</p>
                 </div>
-                <div className='flex flex-col sapce-y-2'>
-                    <p className='text-[#3676E0] font-three font-bold text-3xl md:text-2xl 
-                    lg:text-4xl'>Case Study</p>
-                    <img src={underline} alt="underline" className='w-[12rem] lg:w-[13rem]'/>
+                <div className='flex flex-row lg:space-x-5 space-x-3'>
+                    <div className='flex flex-col space-y-2'>
+                        <p className='text-[#3676E0] font-three font-bold text-2xl md:text-xl 
+                        lg:text-4xl'>Case Study</p>
+                        <img src={underline} alt="underline" className='w-[12rem] lg:w-[13rem]'/>
+                    </div>
+                    <img src={rideCursor} alt="cursor" className='self-end w-[10.5rem] 
+                    md:w-[9rem] lg:w-[12rem] xl:w-auto'/>
                 </div>
-                <img src={rideCursor} alt="cursor" className='absolute left-44 md:left-[11.5rem]
-                 w-[10.5rem] -bottom-5 lg:w-[12rem] lg:left-[16rem] xl::left-[20rem]'/>
             </div>
-            <figure className='relative md:ml-40 xl:ml-0'>
+            <figure className='relative md:ml-5 lg:ml-0 xl:-ml-44'>
             <img src={rideMainOne} alt="main-one" className='w-[60%] md:w-[60%] xl:w-full'/>
             <img src={rideMainTwo} alt="main-two" className='absolute top-20 xl:top-32 
             left-[7.2rem] lg:left-[10.7rem] xl:left-[15.3rem] w-[60%] md:w-[60%] xl:w-full' />

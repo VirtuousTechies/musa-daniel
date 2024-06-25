062b12f631951dbe09b8215cdcda98be47c9ce23
Aik-202 <ikogweassurance85@gmail.com>
2024-06-25 17:10:24 +0100

    Made a few changes

--- musa-daniel/src/Components/FeaturedWork.jsx
+++ musa-daniel/src/Components/FeaturedWork.jsx
@@ -9,10 +9,10 @@ export default function FeaturedWork() {
     <section className='flex flex-col space-y-14 justify-center items-center cursor-pointer'>
         <h2 className='font-bold text-3xl hover:text-[#27AE60]' 
         onClick={() => navigate('/works')}>Featured works</h2>
-        <figure className='relative w-max'>
-          <img src={featuredWorks} alt="featured-works" className='self-center w-[88%]' 
+        <figure className='relative xl:w-max mx-5 xl:mx-0 flex items-center justify-center'>
+          <img src={featuredWorks} alt="featured-works" className='self-center xl:w-[88%]' 
           onClick={() => navigate('/works')} />
-          <div className='absolute top-0 left-0 w-[88%] h-full bg-[#171717] opacity-50'></div>
+          <div className='absolute top-0 left-0 w-full h-full bg-[#171717] opacity-50'></div>
         </figure>
     </section>
   )
